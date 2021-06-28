45454eeafc8e2d1923305d4010a046cb52495475
Michal Polkowski <michalp@speechmatics.com>
2021-06-28 13:35:16 +0200

    api test function

---
+++ pages/api/testme.js
@@ -0,0 +1,8 @@
+
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    // You can call and await an async method here
+    return {
+        body: "Hello, world!"
+    };
+}
