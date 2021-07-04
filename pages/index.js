04aaf3080a8dc8cb364633777245e494763f5e7b
Michal Polkowski <michalp@speechmatics.com>
2021-07-04 14:47:44 +0200

    clean up

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,7 @@ import { projects } from '../utils/projectsData';
 
 const Home = () => (
   <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return <SmallCard key={project.id} Icon={Icon} title={project.name} slug={project.slug} />;
-      })}
-    </div>
+    
   </div>
 );
 
--- pages/project/[path].js
+++ pages/project/[path].js
@@ -16,59 +16,10 @@ function Project({ project, path }) {
   return (
     <div className="project">
       <aside>
-        <h3>You can deploy...</h3>
-        <ul>
-          {projects.map((project) => {
-            return (
-              <li key={project.id}>
-                <a href={`/project/${project.slug}`}>{project.name}</a>
-              </li>
-            );
-          })}
-
-          <li>
-            <a href="/">Home</a>
-          </li>
-        </ul>
+        
       </aside>
       <main>
-        <div className="card-big">
-          <Icon w={249} h={278} />
-          <div className="stats">
-            <div className="stats-details">
-              <div>
-                <StarIcon w={18} h={18} />
-                <p>{project.stargazers_count}</p>
-              </div>
-              <p>stars</p>
-            </div>
-            <div className="stats-details">
-              <div>
-                <WatchIcon w={18} h={18} />
-                <p>{project.watchers_count}</p>
-              </div>
-              <p>watchers</p>
-            </div>
-            <div className="stats-details">
-              <div>
-                <BugIcon w={18} h={18} />
-                <p>{project.open_issues}</p>
-              </div>
-              <p>issues</p>
-            </div>
-          </div>
-          <p className="description">{project.description}</p>
-          <div className="cta">
-            <a
-              className="button-github"
-              href={project.html_url}
-              target="_blank"
-            >
-              <GithubIcon w={24} h={24} />
-              Learn more...
-            </a>
-          </div>
-        </div>
+        
       </main>
     </div>
   );
