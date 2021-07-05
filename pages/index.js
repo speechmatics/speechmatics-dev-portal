fede1ca41d65adadf9b4e2128dfa15621af7907a
Michal Polkowski <michalp@speechmatics.com>
2021-07-05 19:04:39 +0200

    updates

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,15 @@ const Home = () => (
 
     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
 
-    <div>
+    <div style={{ display: 'flex' }}>
+      <div className="shadow_box" style={{ width: '25em', height: '20em' }}>
+        <div>Speechmatics™</div>
+        <div>Freemium</div>
+      </div>
+      <div className="shadow_box" style={{ width: '25em', height: '20em' }}>
+        <div>Speechmatics™</div>
+        <div>Enterprise</div>
+      </div>
 
     </div>
   </div>
