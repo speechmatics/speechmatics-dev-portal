705d7d51681f88b02ab66b9a6d3df953ff221a1d
Michal Polkowski <michalp@speechmatics.com>
2021-07-06 14:22:22 +0200

    login wip 2

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,33 @@
 import { SpeechmaticsLogo } from '../components/Icons';
+import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 
-const Login = () => (
-    <div className="login_container">
+const Login = () => {
+
+    const router = useRouter();
+
+
+
+    return <div className="login_container">
         <SpeechmaticsLogo />
 
 
         <div className="login_text">
-            Please use your credentials to log in.
+            Please use your credentials to log in
         </div>
         <div className="login_form">
-            <input type="text" className="input login"></input>
-            <input type="password" className="input password"></input>
+            <input type="text" placeholder='email' className="input login"></input>
+            <input type="password" placeholder='password' className="input password"></input>
 
-            <button className='next_button'>Log in ➔</button>
+            <button className='next_button' onClick={() => router.push('/dashboard')}>
+                Log in ➔
+            </button>
+        </div>
+        <div className="login_text">
+            or <Link href=''>Create Account</Link>
         </div>
     </div>
-);
+};
 
 export default Login;
