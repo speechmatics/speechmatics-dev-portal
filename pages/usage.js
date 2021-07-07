c23c06bd74b4fac0afdd4c9f126ec5db6adb1214
Michal Polkowski <michalp@speechmatics.com>
2021-07-07 11:22:06 +0200

    build fix

--- pages/usage.js
+++ pages/usage.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Dashboard from '../components/dashboard'
 
 export default function Usage({ }) {
@@ -6,7 +5,7 @@ export default function Usage({ }) {
     return <Dashboard>
         <h1>Usage</h1>
         <div>
-            <Image src='/assets/mock_usage.png' width={600} height={600} />
+            <img src='/assets/mock_usage.png' width={600} height={600} />
         </div>
     </Dashboard>
 }
