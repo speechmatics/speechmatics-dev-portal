9774b359844981d9d638e57066b6d9367ebacd24
Michal Polkowski <michalp@speechmatics.com>
2021-07-12 15:57:30 +0200

    added code example

--- pages/transcribe.js
+++ pages/transcribe.js
@@ -1,6 +1,8 @@
-import Link from 'next/link';
 import Dashboard from '../components/dashboard';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
+import Prism from "prismjs";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+
 
 export default function Transcribe({ }) {
 
@@ -11,6 +13,10 @@ export default function Transcribe({ }) {
     const openFileDialog = () => {
         fileInputRef.current?.click();
     }
+
+    useEffect(() => {
+        Prism.highlightAll();
+    }, []);
 
     return <Dashboard>
         <h1>Submit a file</h1>
@@ -27,5 +33,45 @@ export default function Transcribe({ }) {
             onChange={onSelectFiles}
             accept='video/*,audio/*' />
         <div className='default_button' onClick={openFileDialog}>or choose from the disk</div>
+
+
+        <Tabs p='2em 0em' w='700px'>
+            <TabList>
+                <Tab>Javascript</Tab>
+                <Tab>Python</Tab>
+                <Tab>bash</Tab>
+            </TabList>
+
+            <TabPanels>
+                <TabPanel p={0}>
+                    <pre>
+                        <code className="language-javascript">
+                            {`var ws = new WebSocket('wss://rta:9000/v2');
+ws.binaryType = "arraybuffer";
+ws.onopen = function(event) { onOpen(event) };
+ws.onmessage = function(event) { onMessage(event) };
+ws.onclose = function(event) { onClose(event) };
+ws.onerror = function(event) { onError(event) };
+`}
+                        </code>
+                    </pre>
+                </TabPanel>
+                <TabPanel p={0}>
+                    <pre>
+                        <code className="language-python">
+                            {`python -m smwebsocket.cli --url wss://rta:9000/v2 --max-delay 3 --lang en test.mp3`}
+                        </code>
+                    </pre>
+                </TabPanel>
+                <TabPanel p={0}>
+                    <pre>
+                        <code className="language-bash">
+                            {`curl -L -X POST https://asr.api.speechmatics.com/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
+                        </code>
+                    </pre>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
+
     </Dashboard>
 }
