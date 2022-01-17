8846ac5fcc07aaef280467f1345ab6bda1e777b5
Michal Polkowski <michalp@speechmatics.com>
2022-01-17 20:18:28 +0000

    some tweaks

--- components/dashboard.js
+++ components/dashboard.js
@@ -2,9 +2,8 @@ import { useRouter } from 'next/router'
 import Link from 'next/link';
 import menuData from '../static_data/menu-data'
 import { useContext, useEffect } from 'react';
-import { LoginContext } from '../utils/login-context';
 import { SpeechmaticsLogo, ExternalLink, AccountIcon, LogoutIcon } from '../components/Icons';
-import { Tooltip, Link as ChakraLink } from '@chakra-ui/react';
+import { Tooltip, Link as ChakraLink, Button } from '@chakra-ui/react';
 import { useMsal } from "@azure/msal-react";
 
 
@@ -13,13 +12,15 @@ export default function Dashboard({ children }) {
   const router = useRouter();
 
   const { instance, accounts, inProgress } = useMsal();
+  
+  const account = instance.getActiveAccount();
 
   if (accounts.length == 0) return <div>not logged in, go to <Link href='/login/'>login</Link></div>
 
   return <div className="dashboard_container">
     <div className="dashboard_sidenav">
       <SpeechmaticsLogo w={250} h={130} />
-      <div className='hi_name'>Hi, {"placeholder"}!</div>
+      <div className='hi_name'>Hi, {account.name || account.username}!</div>
       <div className='nav_menu'>
         {menuData.map((item) => <MenuElem item={item} key={item.path}
           selected={router.asPath == item.path} />)}
@@ -41,13 +42,11 @@ export default function Dashboard({ children }) {
           </Tooltip>
         </ChakraLink>
       </Link>
-      <Link href='/login/'>
-        <ChakraLink>
-          <Tooltip label='Log out' placement="left">
-            <div style={{ cursor: 'pointer' }}><LogoutIcon w={30} h={30} /></div>
-          </Tooltip>
-        </ChakraLink>
-      </Link>
+        <Tooltip label='Log out' placement="left">
+          <span style={{ cursor: 'pointer' }} onClick={() => instance.logoutRedirect()}>
+            <LogoutIcon w={30} h={30} />
+          </span>
+        </Tooltip>
     </div>
   </div >
 }
