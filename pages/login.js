89da1d81dd624cc7187c2c4774c5d3310250bc7a
Michal Polkowski <michalp@speechmatics.com>
2022-01-17 15:50:12 +0000

    login user flow

--- pages/login.js
+++ pages/login.js
@@ -1,57 +1,41 @@
 import { SpeechmaticsLogo } from '../components/Icons';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useState, useContext, useEffect } from 'react';
-import { LoginContext } from '../utils/login-context';
+import { useState, useContext, useEffect, useCallback } from 'react';
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "../utils/auth-config";
 
-import mockLogins from '../static_data/mock-logins'
+
 
 export default function Login() {
 
     const router = useRouter();
 
-    const loginContext = useContext(LoginContext);
+    const { instance, accounts, inProgress } = useMsal();
 
-    useEffect(() => {
-        loginContext.clear();
-    }, [])
+    useEffect(()=> {
+        if (accounts.length > 0) setTimeout(() => router.push('/getting-started/'), 1500);
+    }, [instance])
 
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
-    const [wrong, setWrong] = useState(false);
-
-    const attemptLogin = () => {
-        const found = mockLogins.find(el => (`${el.email}@speechmatics.com` == login && el.pass == password));
-        if (found) {
-            router.push('/getting-started/');
-            loginContext.update(found);
+    const LoginSub = () => {
+        if (accounts.length > 0) {
+            return <div className="login_text">You're logged in, let me redirect you...</div> 
+        } else if (inProgress === "login") {
+            return <div className="login_text">Login is currently in progress!</div>
         } else {
-            setWrong(true);
+            return (
+                <div className="login_form">
+                <button className='next_button' onClick={() => instance.loginPopup(loginRequest)}>
+                    Log in / Sign up ➔
+                </button>
+            </div>
+            );
         }
     }
 
     return <div className="login_container">
         <SpeechmaticsLogo />
-
-
-        <div className="login_text">
-            Please use your credentials to log in
-        </div>
-        <div className="login_form">
-            <input type="text" placeholder='email' className="input login"
-                onChange={ev => (setWrong(false), setLogin(ev.target.value))}></input>
-            <input type="password" placeholder='password' className="input password"
-                onChange={ev => (setWrong(false), setPassword(ev.target.value))} ></input>
-
-            {wrong && <div style={{ color: 'firebrick' }}>Used email and password pair does not match.</div>}
-
-            <button className='next_button' onClick={attemptLogin}>
-                Log in ➔
-            </button>
-        </div>
-        <div className="login_text">
-            or <Link href='/create-account/'>Create Account</Link>
-        </div>
+        <LoginSub/>
     </div>
 };
 
