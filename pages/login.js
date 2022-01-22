d4555db06117762a0f206e64beb4c01d056596cf
Michal Polkowski <michalp@speechmatics.com>
2022-01-22 15:53:36 +0000

    popup

--- pages/login.js
+++ pages/login.js
@@ -17,7 +17,7 @@ export default function Login() {
         if (accounts.length > 0) setTimeout(() => router.push('/getting-started/'), 1500);
     }, [accounts, accounts?.length])
 
-    const loginHandler = () => instance.loginRedirect(loginRequest).catch( error => {
+    const loginHandler = () => instance.loginPopup(loginRequest).catch( error => {
         console.log(error)
     })
 
