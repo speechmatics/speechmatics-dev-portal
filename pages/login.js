a65d649f07eda30b50a88432af628e24967482f4
Michal Polkowski <michalp@speechmatics.com>
2022-01-22 15:53:06 +0000

    test

--- pages/login.js
+++ pages/login.js
@@ -40,7 +40,7 @@ export default function Login() {
     return <div className="login_container">
         <SpeechmaticsLogo />
         {process.env.TEST_IF_WORKS_ENV_VAR}
-        redir url: {process.env.REDIRECT_URI}
+        redir url test: {process.env.REDIRECT_URI}
 
         <LoginSub/>
 
