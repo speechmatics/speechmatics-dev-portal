6837405716ba48e9da08e3f3a8790946a939aba3
Michal Polkowski <michalp@speechmatics.com>
2022-01-22 16:30:44 +0000

    redirect consolelog

--- utils/auth-config.js
+++ utils/auth-config.js
@@ -61,6 +61,8 @@ export const msalConfig = {
     }
 };
 
+console.log(`msal auth redirectUri ${msalConfig.auth.redirectUri}`);
+
 /**
  * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
