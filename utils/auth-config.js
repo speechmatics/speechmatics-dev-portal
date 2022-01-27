02b9bf6828739a5e004e3f09f91394839b34ea87
Michal Polkowski <michalp@speechmatics.com>
2022-01-27 00:18:36 +0000

    updated to next 12, removed babel dependency

--- utils/auth-config.js
+++ utils/auth-config.js
@@ -29,7 +29,7 @@ export const msalConfig = {
         authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose SUSI as your default authority.
         knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
         redirectUri: process.env.REDIRECT_URI || "http://localhost:3000/", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-        postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI ||"http://localhost:3000/", // Indicates the page to navigate after logout.
+        postLogoutRedirectUri: process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/", // Indicates the page to navigate after logout.
         navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
     },
     cache: {
@@ -61,13 +61,13 @@ export const msalConfig = {
     }
 };
 
-console.log(`msal auth redirectUri ${msalConfig.auth.redirectUri}`);
+//console.log(`msal auth redirectUri ${msalConfig.auth.redirectUri}`);
 
 /**
  * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
- export const protectedResources = {
+export const protectedResources = {
     apiHello: {
         endpoint: "http://localhost:4010/hello",
         scopes: ["https://testb2cmichalp.onmicrosoft.com/helloapi/demo.read"], // e.g. api://xxxxxx/access_as_user
@@ -80,6 +80,6 @@ console.log(`msal auth redirectUri ${msalConfig.auth.redirectUri}`);
  * For more information about OIDC scopes, visit: 
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
- export const loginRequest = {
+export const loginRequest = {
     scopes: [...protectedResources.apiHello.scopes]
 };
