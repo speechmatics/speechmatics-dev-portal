a135014bdd252e6ad5e2917e5229795d06d5bd17
Michal Polkowski <michalp@speechmatics.com>
2022-02-02 12:46:59 +0000

    changes

--- components/call-test.tsx
+++ components/call-test.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@chakra-ui/react";
 import { useCallback, useContext, useEffect, useState } from "react";
-import { callApiWithToken } from '../utils/call-api'
+import { callApiWithToken } from "../utils/call-api";
 
-
-export default ({tokenPayload}) => {
-
+export default ({ tokenPayload }) => {
   const [response, setResponse] = useState();
 
   const callb = useCallback(() => {
@@ -22,16 +20,21 @@ export default ({tokenPayload}) => {
   const callb2 = useCallback(() => {
     if (tokenPayload) {
       console.log({ tokenPayload });
-      callApiWithToken(tokenPayload.idToken, process.env.TEST_API_CALL_ENDPOINT).then(
+      callApiWithToken(
+        tokenPayload.idToken,
+        process.env.TEST_API_CALL_ENDPOINT
+      ).then(
         (resp) => (setResponse(resp.name), console.log(JSON.stringify(resp)))
       );
     }
   }, [tokenPayload]);
 
-  return <>
-  <Button onClick={callb}>test accesstoken</Button>
-        <Button onClick={callb2}>test idtoken</Button>
-        <div dangerouslySetInnerHTML={{ __html: response }} />; test::{" "}
-        {process.env.TEST_IF_WORKS_ENV_VAR}
+  return (
+    <>
+      <Button onClick={callb}>test accesstoken</Button>
+      <Button onClick={callb2}>test idtoken</Button>
+      <div dangerouslySetInnerHTML={{ __html: response }} />; test::{" "}
+      {process.env.TEST_IF_WORKS_ENV_VAR}
     </>
-}
+  );
+};
