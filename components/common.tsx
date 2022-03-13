daca08a34f15ba2c3fb1436c3f6284614f299091
Michal Polkowski <michalp@speechmatics.com>
2022-03-13 23:50:40 +0000

    runtime url in code example

--- components/common.tsx
+++ components/common.tsx
@@ -13,9 +13,11 @@ import {
   Tooltip,
   VStack,
 } from '@chakra-ui/react';
+import { useContext } from 'react';
 import { IoCopyOutline } from 'react-icons/io5';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { nord as codeTheme } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import accountStoreContext from '../utils/account-store-context';
 
 export const SmPanel = ({ children, ...props }) => (
   <VStack className="sm_panel" alignItems="flex-start" {...props}>
@@ -63,7 +65,10 @@ export const PageHeader = ({ headerLabel, introduction }) => {
     </>
   );
 };
-export const CodeExamples = ({}) => {
+export const CodeExamples = ({ }) => {
+
+  const { accountStore } = useContext(accountStoreContext);
+
   return (
     <Tabs size="lg" variant="speechmatics" mt="1em">
       <TabList marginBottom="-1px">
@@ -74,17 +79,17 @@ export const CodeExamples = ({}) => {
       <TabPanels>
         <TabPanel width="750px">
           <CodeHighlight
-            code={`curl -L -X POST https://asr.api.speechmatics.com/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
+            code={`curl -L -X POST ${accountStore.getRuntimeURL() || '$HOST'}/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
           />
         </TabPanel>
         <TabPanel width="750px">
           <CodeHighlight
-            code={`/* mac */ curl -L -X POST https://asr.api.speechmatics.com/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
+            code={`/* mac */ curl -L -X POST ${accountStore.getRuntimeURL()}/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
           />
         </TabPanel>
         <TabPanel width="750px">
           <CodeHighlight
-            code={`/* linux */ curl -L -X POST https://asr.api.speechmatics.com/v2/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
+            code={`/* linux */ curl -L -X POST ${accountStore.getRuntimeURL()}/jobs/ -H "Authorization: Bearer NDFjOTE3NGEtOWVm" -F data_file=@example.wav -F config="$(cat config.json)" | jq`}
           />
         </TabPanel>
       </TabPanels>
