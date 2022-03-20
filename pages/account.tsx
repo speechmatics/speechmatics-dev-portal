4603fa6e24fc88217bdda22b243c252e999d39d8
Michal Polkowski <michalp@speechmatics.com>
2022-03-20 16:19:40 +0000

    account remove contents

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,39 +1,10 @@
 import Dashboard from '../components/dashboard';
 
-export default function Account({}) {
+export default function Account({ }) {
   return (
     <Dashboard>
       <h1>Account</h1>
-      <div className="account_forms">
-        <section>
-          <div>
-            <input type="text" placeholder="title" value={''}></input>
-            <input type="text" placeholder="First name" value={''}></input>
-            <input type="text" placeholder="Surname"></input>
-          </div>
-          <div>
-            <input type="text" placeholder="email" value={``}></input>
-          </div>
-          <div>
-            <input type="text" placeholder="phone number"></input>
-          </div>
-          <div className="default_button">Apply changes</div>
-        </section>
-        <section>
-          <div>
-            <input type="text" placeholder="new password"></input>
-            <input type="text" placeholder="repeat new password"></input>
-          </div>
-          <div className="default_button">Submit password changes</div>
-        </section>
-        <section>
-          <div className="description_text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
-          </div>
-          <div className="default_button red">Remove account</div>
-        </section>
-      </div>
+
     </Dashboard>
   );
 }
