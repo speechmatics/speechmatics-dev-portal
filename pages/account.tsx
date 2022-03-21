49a7c6958c16bac2dc2efc36a11172f7cecb42c8
Michal Polkowski <michalp@speechmatics.com>
2022-03-21 16:01:35 +0000

    password reset test1

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,10 +1,30 @@
+import { useMsal } from '@azure/msal-react';
+import { Button } from '@chakra-ui/react';
+import { DescriptionLabel, PageHeader } from '../components/common';
 import Dashboard from '../components/dashboard';
+import { b2cPolicies } from '../utils/auth-config';
 
-export default function Account({ }) {
+export default function Account({}) {
+  const { instance, inProgress } = useMsal();
+
+  const resetPassword = () => {
+    instance
+      .loginPopup({
+        scopes: [],
+        authority: b2cPolicies.authorities.forgotPassword.authority,
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <Dashboard>
-      <h1>Account</h1>
+      <PageHeader headerLabel="Account" introduction="" />
 
+      <DescriptionLabel>Reset Your password:</DescriptionLabel>
+      <Button variant="speechmatics" onClick={resetPassword}>
+        Reset password
+      </Button>
     </Dashboard>
   );
 }
