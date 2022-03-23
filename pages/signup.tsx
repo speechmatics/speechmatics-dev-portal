1e7f945e68680d4177438696683cb2e4e885a5bf
Michal Polkowski <michalp@speechmatics.com>
2022-03-23 18:13:48 +0000

    changed to 1s

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -60,7 +60,7 @@ export default function SignUp() {
           .catch((error) => {
             console.log(error);
           }),
-      5000
+      1000
     );
 
     return () => window.clearTimeout(st);
