3b39e8a9546a42374538b9d77f551f13bdf242f9
Michal Polkowski <michalp@speechmatics.com>
2022-03-24 11:12:17 +0000

    home section sizing

--- pages/home.tsx
+++ pages/home.tsx
@@ -14,53 +14,55 @@ export default function Home({ }) {
   return (
     <Dashboard>
       <PageHeader headerLabel="Home" introduction="Welcome to Speechmatics Cloud Service" />
-      <HStack alignItems="flex-start" spacing="2em">
-        <HomeBox
-          bgColor="smBlue.500"
-          icon={<TerminalGraphIcon />}
-          text="Transcribe an audio file with code"
-          buttonLabel="Get Started"
-          hrefUrl="/getting-started/"
-        />
-        <HomeBox
-          bgColor="smGreen.500"
-          icon={<CloudUploadIcon />}
-          iconPadding="20px"
-          text="Upload and transcribe an audio file"
-          buttonLabel="Transcribe Now"
-          hrefUrl="/getting-started/"
-        />
-      </HStack>
-      <HStack alignItems="flex-start" spacing="2em" mt="2em">
-        <HomeWhiteBox
-          icon={<PadLockOpenIcon />}
-          title="Manage API Keys"
-          description="You need to create an API key to make API requests."
-          buttonLabel="Create API key"
-          hrefUrl="/manage-access/"
-        />
-        <HomeWhiteBox
-          icon={<LookingGlassChartIcon />}
-          title="Track your usage"
-          description="Usage is measured in hours of audio processed"
-          buttonLabel="View Usage"
-          hrefUrl="/usage/"
-        />
-        <HomeWhiteBox
-          icon={<LearnBookIcon />}
-          title="Learning Resources"
-          description="Explore our documentation and learning resources"
-          buttonLabel="Learn"
-          hrefUrl="/learn/"
-        />
-      </HStack>
+      <VStack spacing='2em' width='800px'>
+        <HStack alignItems="flex-start" spacing="2em">
+          <HomeBox
+            bgColor="smBlue.500"
+            icon={<TerminalGraphIcon />}
+            text="Transcribe an audio file with code"
+            buttonLabel="Get Started"
+            hrefUrl="/getting-started/"
+          />
+          <HomeBox
+            bgColor="smGreen.500"
+            icon={<CloudUploadIcon />}
+            iconPadding="20px"
+            text="Upload and transcribe an audio file"
+            buttonLabel="Transcribe Now"
+            hrefUrl="/getting-started/"
+          />
+        </HStack>
+        <HStack alignItems="flex-start" spacing="2em">
+          <HomeWhiteBox
+            icon={<PadLockOpenIcon />}
+            title="Manage API Keys"
+            description="You need to create an API key to make API requests."
+            buttonLabel="Create API key"
+            hrefUrl="/manage-access/"
+          />
+          <HomeWhiteBox
+            icon={<LookingGlassChartIcon />}
+            title="Track your usage"
+            description="Usage is measured in hours of audio processed"
+            buttonLabel="View Usage"
+            hrefUrl="/usage/"
+          />
+          <HomeWhiteBox
+            icon={<LearnBookIcon />}
+            title="Learning Resources"
+            description="Explore our documentation and learning resources"
+            buttonLabel="Learn"
+            hrefUrl="/learn/"
+          />
+        </HStack>
+      </VStack>
     </Dashboard>
   );
 }
 
 const HomeBox = ({ bgColor, icon, iconPadding = '24px', text, buttonLabel, hrefUrl }) => {
   return (
-    <VStack width="376px" height="288px" bg={bgColor} borderRadius="2px" p="1.5em" spacing="1em">
+    <VStack width="100%" height="288px" bg={bgColor} borderRadius="2px" p="1.5em" spacing="1em">
       <Box borderRadius={'100%'} width="88px" height="88px" bg="smWhite.150" p={iconPadding}>
         {icon}
       </Box>
@@ -87,7 +89,7 @@ const HomeWhiteBox = ({ icon, title, description, buttonLabel, hrefUrl }) => {
   return (
     <VStack
       className="sm_panel"
-      width="240px"
+      width="100%"
       height="278px"
       alignItems="center"
       padding="2em"
