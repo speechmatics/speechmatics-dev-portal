d47e4665f6fe832e2e18c1d11c73065bf7d78993
Michal Polkowski <michalp@speechmatics.com>
2022-03-28 17:09:20 +0100

    reset pass

--- pages/account.tsx
+++ pages/account.tsx
@@ -2,20 +2,19 @@ import { useMsal } from '@azure/msal-react';
 import { Button } from '@chakra-ui/react';
 import { DescriptionLabel, PageHeader } from '../components/common';
 import Dashboard from '../components/dashboard';
-import { b2cPolicies } from '../utils/auth-config';
 
 export default function Account({ }) {
   const { instance, inProgress } = useMsal();
 
-  // const resetPassword = () => {
-  //   instance
-  //     .loginPopup({
-  //       scopes: [],
-  //       authority: b2cPolicies.authorities.forgotPassword.authority,
-  //     })
-  //     .then((response) => console.log(response))
-  //     .catch((error) => console.error(error));
-  // };
+  const resetPassword = () => {
+    instance
+      .loginRedirect({
+        scopes: [],
+        authority: "https://speechmaticsb2c.b2clogin.com/speechmaticsb2c.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_PASSWORDRESET",
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
 
   return (
     <Dashboard>
