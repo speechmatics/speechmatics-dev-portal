f350d4b67a2147db8ad1ad76b6dee7e6f6cf8f36
Michal Polkowski <michalp@speechmatics.com>
2022-04-11 14:38:12 +0100

    removed apostrophe

--- pages/home.tsx
+++ pages/home.tsx
@@ -16,7 +16,7 @@ import {
 export default function Home({ }) {
   return (
     <Dashboard>
-      <PageHeader headerLabel="Home" introduction="Welcome to Speechmatics’ SaaS Portal." />
+      <PageHeader headerLabel="Home" introduction="Welcome to Speechmatics SaaS Portal." />
       <VStack spacing='2em' width='800px'>
         <InfoBarbox
           width="100%"
