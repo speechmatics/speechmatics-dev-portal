3572ad28dd4118497852ac06b0e2f60ab63e9db6
Michal Polkowski <michalp@speechmatics.com>
2022-04-12 08:11:42 +0100

    comma removed

--- components/common.tsx
+++ components/common.tsx
@@ -196,7 +196,7 @@ export const CodeExamples = observer(({ token }: { token?: string }) => {
               code={`curl.exe -L -X POST ${accountStore.getRuntimeURL() || '$HOST'}/v2/jobs/ -H "Authorization: Bearer ${token || `Ex4MPl370k3n`
                 }" -F data_file=@example.wav -F config="{\\"type\\": \\"transcription\\", \\"transcription_config\\": { \\"operating_point\\":\\"enhanced\\", \\"language\\": \\"en\\" }}"`}
             />
-            <DescriptionLabel pt='2em'>Get a transcript, using the job ID returned by the POST request above:</DescriptionLabel>
+            <DescriptionLabel pt='2em'>Get a transcript using the job ID returned by the POST request above:</DescriptionLabel>
             <CodeHighlight
               code={`curl.exe -L -X GET ${accountStore.getRuntimeURL() || '$HOST'}/v2/jobs/INSERT_JOB_ID/transcript -H "Authorization: Bearer ${token || `Ex4MPl370k3n`
                 }"`}
@@ -210,7 +210,7 @@ export const CodeExamples = observer(({ token }: { token?: string }) => {
                 }" -F data_file=@example.wav -F config='{"type": "transcription","transcription_config": { "operating_point":"enhanced", "language": "en" }}'`}
             />
 
-            <DescriptionLabel pt='2em'>Get a transcript, using the job ID returned by the POST request above:</DescriptionLabel>
+            <DescriptionLabel pt='2em'>Get a transcript using the job ID returned by the POST request above:</DescriptionLabel>
             <CodeHighlight
               code={`curl -L -X GET ${accountStore.getRuntimeURL() || '$HOST'}/v2/jobs/INSERT_JOB_ID/transcript -H "Authorization: Bearer ${token || `Ex4MPl370k3n`
                 }"`}
