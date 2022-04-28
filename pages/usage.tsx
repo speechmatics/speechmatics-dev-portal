a5f2788e85482ece96eadf7cb1847236c7a67651
Tudor Evans <tudore@speechmatics.com>
2022-04-28 15:15:20 +0100

    fix missing code problem and 100% width text

--- pages/usage.tsx
+++ pages/usage.tsx
@@ -190,7 +190,6 @@ const UsageBreakdownGrid = ({ data, isLoading }) => (
     <GridItem className="grid_header">Hours Used</GridItem>
 
     {data?.map((el: UsageUnit, i: number) => {
-      console.log(el.since)
       return (
         <React.Fragment key={el.since}>
           <GridItem className="grid_row_divider">{i != 0 && <hr />}</GridItem>
