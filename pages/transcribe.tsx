28c2fd2382f346fd67d87e5a85ad7eebdd904616
Michal Polkowski <michalp@speechmatics.com>
2022-05-11 20:09:42 +0100

    components refactor

--- pages/transcribe.tsx
+++ pages/transcribe.tsx
@@ -4,18 +4,30 @@ import { DescriptionLabel, HeaderLabel, PageHeader, SmPanel } from "../component
 import Dashboard from "../components/dashboard";
 import { QuestionmarkInCircle } from "../components/icons-library";
 
+const languagesData = [
+  { label: 'English', value: 'en' },
+  { label: 'French', value: 'fr' },
+  { label: 'German', value: 'de' },
+]
 
+const separation = [
+  { label: 'None', value: 'none' },
+  { label: 'Speaker', value: 'speaker' },
+
+]
 
 export default observer(function Transcribe({ }) {
   return (
     <Dashboard>
       <PageHeader headerLabel="Transcribe" introduction="Use our online tool to transcribe your audio." />
       <SmPanel width='100%' maxWidth='900px'>
-        <HeaderLabel>Upload a File</HeaderLabel>
-        <DescriptionLabel>The audio file can be wav, mp3 or any compatible format.</DescriptionLabel>
-        <Box alignSelf='stretch' pt={5} pb={3}>
-          {/* drop file component */}
-          <Box alignSelf='stretch' height='100px' bgColor='smBlack.120' style={{ border: '1px dashed #DDDDDD' }}></Box>
+        <Box width='100%'>
+          <HeaderLabel>Upload a File</HeaderLabel>
+          <DescriptionLabel>The audio file can be wav, mp3 or any compatible format.</DescriptionLabel>
+          <Box alignSelf='stretch' pt={5} pb={3}>
+            {/* drop file component */}
+            <Box alignSelf='stretch' height='100px' bgColor='smBlack.120' style={{ border: '1px dashed #DDDDDD' }}></Box>
+          </Box>
         </Box>
         <Divider />
         <Box py={5} width='100%'>
@@ -23,39 +35,10 @@ export default observer(function Transcribe({ }) {
           <DescriptionLabel>Choose the best features to suit your transcription requirements.</DescriptionLabel>
 
           <Flex width='100%' wrap='wrap' gap={6}>
-            <Box flex='1 0 40%'>
-              <HStack alignItems='center' pb={2}>
-                <Box color='smBlack.400'>Language</Box>
-                <Box><Tooltip label='Expected language of transcription' hasArrow placement="right"><Box><QuestionmarkInCircle /></Box></Tooltip></Box>
-              </HStack>
-              <Select borderColor='smBlack.170' color='smBlack.300' borderRadius='2px' size='lg'>
-                <option value='en' selected>English</option>
-                <option value='fr' >French</option>
-                <option value='de' >German</option>
-                <option value='pl' >Polish</option>
-              </Select>
-            </Box>
-            <Box flex='1 0 40%'>
-              <HStack alignItems='center' pb={2}>
-                <Box color='smBlack.400'>Separation</Box>
-                <Box><Tooltip label='Separation of transcription' hasArrow placement="right"><Box><QuestionmarkInCircle /></Box></Tooltip></Box>
-              </HStack>
-              <Select borderColor='smBlack.170' color='smBlack.300' borderRadius='2px' size='lg'>
-                <option value='none' >None</option>
-                <option value='speaker' selected>Speaker</option>
-              </Select>
-            </Box>
-            <Box flex='1 0 40%' maxWidth='calc(50% - 0.85em)'>
-              <HStack alignItems='center' pb={2}>
-                <Box color='smBlack.400'>Accuracy</Box>
-                <Box><Tooltip label='Expected accuracy of transcription' hasArrow placement="right"><Box><QuestionmarkInCircle /></Box></Tooltip></Box>
-              </HStack>
-              <Flex width='100%'>
-                <Button variant='speechmatics' height='3em' py='1.4em' fontFamily='RMNeue-Regular' flex='1'>Enhanced</Button>
-                <Button variant='speechmaticsOutline' height='3em' border='1px solid' borderColor='smBlack.180'
-                  color='smBlack.250' fontFamily='RMNeue-Light' flex='1'>Standard</Button>
-              </Flex>
-            </Box>
+            <SelectField label="Language" tooltip='Expected language of transcription' data={languagesData} />
+            <SelectField label="Separation" tooltip='Separation of transcription' data={separation} />
+            <ChoiceButtons label="Accuracy" tooltip="Accuracy model" />
+
           </Flex>
         </Box>
         <Divider />
@@ -68,3 +51,37 @@ export default observer(function Transcribe({ }) {
     </Dashboard>
   );
 })
+
+
+type SelectFieldProps = {
+  label: string,
+  tooltip: string,
+  data: { value: string, label: string }[]
+}
+
+const SelectField = ({ label, tooltip, data }: SelectFieldProps) => {
+  return <Box flex='1 0 40%'>
+    <HStack alignItems='center' pb={2}>
+      <Box color='smBlack.400'>{label}</Box>
+      <Box><Tooltip label={tooltip} hasArrow placement="right"><Box><QuestionmarkInCircle /></Box></Tooltip></Box>
+    </HStack>
+    <Select borderColor='smBlack.170' color='smBlack.300' borderRadius='2px' size='lg'>
+      {data.map(({ value, label }, i) => <option value={value} selected={i == 0}>{label}</option>)}
+    </Select>
+  </Box>
+}
+
+
+const ChoiceButtons = ({ label, tooltip }) => {
+  return <Box flex='1 0 40%' maxWidth='calc(50% - 0.85em)'>
+    <HStack alignItems='center' pb={2}>
+      <Box color='smBlack.400'>{label}</Box>
+      <Box><Tooltip label={tooltip} hasArrow placement="right"><Box><QuestionmarkInCircle /></Box></Tooltip></Box>
+    </HStack>
+    <Flex width='100%'>
+      <Button variant='speechmatics' height='3em' py='1.4em' fontFamily='RMNeue-Regular' flex='1'>Enhanced</Button>
+      <Button variant='speechmaticsOutline' height='3em' border='1px solid' borderColor='smBlack.180'
+        color='smBlack.250' fontFamily='RMNeue-Light' flex='1'>Standard</Button>
+    </Flex>
+  </Box>
+}
