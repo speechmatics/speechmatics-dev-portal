ee3555a63150efe66fd5ebbeae25e3f83054a059
Nelson Kondia <nelsonk@speechmatics.com>
2022-05-27 09:56:52 +0100

    Increase length following change in request validation

--- pages/manage-access.tsx
+++ pages/manage-access.tsx
@@ -161,7 +161,7 @@ export const GenerateTokenComponent: ChakraComponent<'div', GTCprops>
                   disabled={genTokenStage == 'waiting'}
                   onKeyDown={inputOnKeyDown}
                   data-qa="input-token-name"
-                  maxLength={30}
+                  maxLength={120}
                 ></Input>
                 <Button
                   variant="speechmatics"
