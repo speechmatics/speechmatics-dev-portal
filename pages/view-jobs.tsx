f2b1865a7b6680f549574cfcb5fb2fada7313856
Tudor Evans <tudore@speechmatics.com>
2022-06-08 16:19:57 +0100

    update responsiveness for different browsers

--- pages/view-jobs.tsx
+++ pages/view-jobs.tsx
@@ -3,13 +3,15 @@ import Dashboard from '../components/dashboard';
 import { RecentJobs } from '../components/recent-jobs';
 
 export default function ViewJobs() {
+
   return (
     <Dashboard>
       <PageHeader headerLabel="View Jobs" introduction="See Your Recent Jobs." />
       <SmPanel
-        mx={{ base: 2, md: 0 }}
-        width={{ base: '98vw', md: '100vw' }}
-        maxWidth={{ base: '360px', sm: '400px', md: '420px', lg: '600px', xl: '900px' }}
+        alignSelf="center"
+        mx={{ base: '2vw', sm: 0, md: 0 }}
+        width={{ base: '96vw', sm: '100%', md: '100%', lg: '100%' }}
+        maxWidth={{ sm: '450px', md: '480px', lg: '700px', xl: '900px' }}
       >
         <RecentJobs />
       </SmPanel>
