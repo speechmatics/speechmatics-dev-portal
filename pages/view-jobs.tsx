a660e66047f9debf26542eb8747775b43113acb9
Tudor Evans <tudore@speechmatics.com>
2022-06-09 10:02:40 +0100

    improve SmPanel responsiveness

--- pages/view-jobs.tsx
+++ pages/view-jobs.tsx
@@ -7,12 +7,7 @@ export default function ViewJobs() {
   return (
     <Dashboard>
       <PageHeader headerLabel="View Jobs" introduction="See Your Recent Jobs." />
-      <SmPanel
-        alignSelf="center"
-        mx={{ base: '1vw', sm: 0, md: 0 }}
-        width={{ base: '94vw', sm: '100%', md: '100%', lg: '100%' }}
-        maxWidth={{ sm: '450px', md: '480px', lg: '700px', xl: '900px' }}
-      >
+      <SmPanel width='100%' maxWidth='900px'>
         <RecentJobs />
       </SmPanel>
     </Dashboard>
