53c0728853c4b3b64beff2ec4d3c1c31e5fefcd2
Tudor Evans <tudore@speechmatics.com>
2022-06-10 15:03:28 +0100

    change upload copy

--- components/common.tsx
+++ components/common.tsx
@@ -498,7 +498,9 @@ export const positiveToast = (descr: string) =>
 
 export const AttentionBar = ({ description, data_qa = 'attentionBar', centered = false }) => (
   <HStack width="100%" bg="smRed.100" p="1em" spacing="1em" justifyContent={centered ? 'center' : ''}>
-    <ExclamationIcon />
+    <Box>
+      <ExclamationIcon />
+    </Box>
     <Text as='span' data-qa={data_qa} color="smRed.500" fontSize="0.95em" flex={centered ? "" : "1"} >
       {description}
     </Text>
